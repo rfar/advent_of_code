d065f3b3bf80c2f683cde40fe8ce37d4b1a8fdbd
r.farhang <pq_62@yahoo.com>
2018-07-17 11:17:13 +0430

    Puzzle #1 solved and verified

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -2,10 +2,27 @@
 
 (defn find-repeated
   [input]
-  (if (= (first input) (second input))
-    (cons (first input) find-repeated)
-    (find-repeated)))
+  (if (empty? input)
+    input
+    (if (= (first input) (second input))
+      (cons (first input) (find-repeated (rest input)))
+      (find-repeated (rest input)))))
 
-(defn sum-repeated
-  [rep]
-  (reduce + rep))
+;; (defn sum-repeated
+;;   [rep]
+;;   (reduce + rep))
+
+;; (defn read-file
+;;   [file-name]
+;;   (slurp file-name))
+
+(defn puzzle-1 []
+  (reduce + (map read-string
+                 (map str
+                      (find-repeated (slurp "resources/input-01.txt"))))))
+
+(+ 2 (puzzle-1)) ;; 2 is the last number, since it's equal to the first one
+
+;; (map str '(\3 \4 \5))
+;; (read-string "3")
+;; (str \5)
