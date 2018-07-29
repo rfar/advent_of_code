b6dd96392dabf1f738cdc89cba2f4337d9797975
r.farhang <pq_62@yahoo.com>
2018-07-29 19:55:33 +0430

    Implement partial solution to Puzzle #03

--- src/advent_of_code/puzzle-03.clj
+++ src/advent_of_code/puzzle-03.clj
@@ -1,6 +1,23 @@
 (ns advent-of-code.core
   (:require [clojure.string :as str]))
 
+;; Caculated from `4 * l * (l + 1) >= n` which reduces to: `l >= (sqrt(n) - 1) / 2`
+(defn loop-num [n]
+  "Calculates the loop number containing the number n"
+  (-> n
+      Math/sqrt
+      dec
+      (/ 2)
+      Math/floor))
+;;   (Math/floor (/ (dec (Math/sqrt n)) 2)))
+
+(defn offset-from-center [n l]
+  "Calculates the offset of the number n from the loop's center element
+  irrespective of which side of the loop it is situated")
+
+(defn solve [n]
+  (loop-num n))
+
 (solve 265149)
 
 ;; Playground
@@ -11,3 +28,5 @@
 ;; center of loop's edge.
 ;; Actually we'd better calculate the formula for the center number on each
 ;; of the four edges of each loop.
+(quot 11 2)
+(dec (Math/sqrt 81))
