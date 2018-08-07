4ec4e05cdb0d977360c710f39e15b2275b934345
r.farhang <pq_62@yahoo.com>
2018-08-07 16:04:44 +0430

    Resolve stack overflow using `loop/recur` and solve Puzzle #05

--- src/advent_of_code/puzzle-05.clj
+++ src/advent_of_code/puzzle-05.clj
@@ -1,25 +1,32 @@
 (ns advent-of-code.core
   (:require [clojure.string :as str]))
 
-(defn update-jump-count [jc js]
-  (if (zero? js)
-    jc
-    (inc jc)))
+;; (defn update-jump-count [jc js]
+;;   (if (zero? js)
+;;     jc
+;;     (inc jc)))
 
 (defn jump [i jc v]
   (let [js (get v i)
         t (+ i js)]
-    (if (>= t (count v))
-      (inc jc)
-      (jump t (update-jump-count jc js) (update v i inc)))))
+    (do
+      (println v)
+      (if (>= t (count v))
+        (inc jc)
+        (jump t (inc jc) (update v i inc))))))
 
 (defn solve [f]
-  (let [v (vec (map read-string (str/split-lines (slurp f))))]
-    (jump 0 1 v)))
+  (let [nums (vec (map read-string (str/split-lines (slurp f))))]
+    (loop [i 0 jc 0 v nums]
+      (let [js (get v i)
+            t (+ i js)]
+        (if (>= t (count v))
+          (inc jc)
+          (recur t (inc jc) (update v i inc)))))))
 
 (solve "resources/input-05.txt")
 
 ;; Playground
-;; (jump 0 1 [0 3 0 1 -3])
+(jump 0 0 [0 3 0 1 -3])
 (update [0 1 2 3] 2 #((constantly -1) %))
 (get [0 1 3 8] 2)
